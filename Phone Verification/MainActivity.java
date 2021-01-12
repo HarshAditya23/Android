f39490230673c28cf80685fa4e4a6d066c5422ca
HarshAditya23 <34307686+HarshAditya23@users.noreply.github.com>
2021-01-12 23:48:43 +0530

    Add files via upload

---
+++ Phone Verification/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.android.phoneverification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.phone);
+        findViewById(R.id.Continue).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String number = editText.getText().toString();
+                if(number.isEmpty() || number.length()<10)
+                {
+                    editText.setError("Valid number is required.");
+                    editText.requestFocus();
+                    return;
+                }
+                String phonenumber = "+91"+number;
+                Intent intent = new Intent(MainActivity.this,VerifyPhone.class);
+                intent.putExtra("phonenumber",phonenumber);
+                startActivity(intent);
+
+            }
+        });
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
+        if (FirebaseAuth.getInstance().getCurrentUser()!=null)
+        {
+            Intent intent = new Intent(this, ProfileActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        }
+    }
+}
